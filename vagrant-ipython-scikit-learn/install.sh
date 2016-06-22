1d06e2afb8e6311d0e3ecfea7975eb2860418ac2
Remi Tassing <remi.tassing@ericsson.com>
2016-06-22 11:02:08 +0200

    update to jupyter

--- vagrant-ipython-scikit-learn/install.sh
+++ vagrant-ipython-scikit-learn/install.sh
@@ -2,6 +2,11 @@
 
 apt-get update
 
-apt-get install -y python-pip python-dev python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas
+apt-get install -y python-pip python-dev python-numpy python-scipy python-matplotlib python-pandas
+
+# install ipython2 ... see "Data Science box (DSbox)"
+sudo pip install jupyter
+
+sudo python2 -m ipykernel install
 
 pip install -U scikit-learn
